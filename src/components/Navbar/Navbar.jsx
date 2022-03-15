91b564964a49c7869b06a69e7a52271fd7be4091
travka47 <travka.eal@students.dvfu.ru>
2022-03-15 19:25:45 +1000

    NavLink instead of a

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
-      <a href="/profile">Profile</a>
-      <a href="/messages">Messages</a>
-      <a href="/news">News</a>
-      <a href="/music">Music</a>
-      <a href="/settings">Settings</a>
+      <NavLink to="/profile" className={ navData => navData.isActive ? classes.active : classes.inactive}>Profile</NavLink>
+      <NavLink to="/messages" className={ navData => navData.isActive ? classes.active : classes.inactive}>Messages</NavLink>
+      <NavLink to="/news" className={ navData => navData.isActive ? classes.active : classes.inactive}>News</NavLink>
+      <NavLink to="/music" className={ navData => navData.isActive ? classes.active : classes.inactive}>Music</NavLink>
+      <NavLink to="/settings" className={ navData => navData.isActive ? classes.active : classes.inactive}>Settings</NavLink>
     </nav>
   );
 };
