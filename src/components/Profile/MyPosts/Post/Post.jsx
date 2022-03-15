dc1eae13c3d90147bae745f84e80c3017e50a36b
travka47 <travka.eal@students.dvfu.ru>
2022-03-15 19:26:45 +1000

    naming fix

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,7 +5,7 @@ const Post = (props) => {
     return (
         <div className={classes.post}>
             <img className={classes.avatar} src="https://i1.wp.com/s1.r29static.com/bin/entry/0e2/x,80/1894701/image.jpg" alt='avatar'></img>
-            <div className={classes.infobox}>
+            <div className={classes.info}>
                 <p><span>Timothee Chalamet</span>, SomeOne Famous</p>
                 <p className={classes.text}>{props.message}</p>
             </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,19 +2,19 @@ import React from "react"
 import classes from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts"
 
-const Profile = () => {
+const Profile = () => { 
     return (
-        <div className={classes.content}>
-            <div className={classes.profile}>
-                <div className={classes.card}>
-                    <div className={classes.avatar}>
+        <div>
+            <div className={classes.user}>
+                <div className={classes.polaroid}>
+                    <div className={classes.userPhoto}>
                         <img src="https://i1.wp.com/s1.r29static.com/bin/entry/0e2/x,80/1894701/image.jpg" alt='avatar'></img>
                     </div>
                     <div className={classes.details}>
                         <h2>SomeOne Famous<span>Timothee Chalamet</span></h2>
                     </div>
                 </div>
-                <div className={classes.infobox}>
+                <div className={classes.userInfo}>
                     <ul>
                         <li>Date of birth: <span>04.07.2002</span></li>
                         <li>City: <span>Vladivostok</span></li>
