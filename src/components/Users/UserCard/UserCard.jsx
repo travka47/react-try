dd82c379ffce6ebefd44aad5a83b0d118adf114a
travka47 <travka.eal@students.dvfu.ru>
2022-03-30 23:46:12 +1000

    styled the users page

--- src/components/Users/UserCard/UserCard.jsx
+++ src/components/Users/UserCard/UserCard.jsx
@@ -3,11 +3,22 @@ import classes from "./UserCard.module.css";
 
 const UserCard = (props) => {
   return (
-    <div className={classes.card}>
-      <img
-        src="https://i1.wp.com/s1.r29static.com/bin/entry/0e2/x,80/1894701/image.jpg"
-        alt="avatar"
+    <div className={classes.userCard}>
+      <img className={classes.userPhoto}
+           src={props.user.photos.small != null ? props.user.photos.small : "https://i1.wp.com/s1.r29static.com/bin/entry/0e2/x,80/1894701/image.jpg"}
+           alt="avatar"
       />
+      <p className={classes.userName}>{props.user.name}</p>
+      <p
+        className={classes.userStatus}>{props.user.status != null ? props.user.status : "status status status status status status status status"}</p>
+      {props.user.followed
+        ? <button className={classes.unfollowed} onClick={() => {
+          this.props.unfollow(props.user.id)
+        }}>Unfollow</button>
+        : <button className={classes.followButton} onClick={() => {
+          this.props.follow(props.user.id)
+        }}>Follow</button>
+      }
     </div>
   )
 };
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./Users.module.css";
 import axios from "axios";
+import UserCard from "./UserCard/UserCard";
 
 class Users extends React.Component {
   componentDidMount() {
@@ -8,28 +9,23 @@ class Users extends React.Component {
       this.props.setUsers(response.data.items)
     })
   }
+
   render() {
     return (
-      <div>
-        {
-          this.props.userData.map(user => <div key={user.id}>
-              <img className={classes.userPhoto}
-                   src={user.photos.small != null ? user.photos.small : "https://i1.wp.com/s1.r29static.com/bin/entry/0e2/x,80/1894701/image.jpg"}
-                   alt="avatar"
-              />
-              <p>{user.name}</p>
-              <p>{user.status}</p>
-              {user.followed
-                ? <button onClick={() => {
-                  this.props.unfollow(user.id)
-                }}>Unfollow</button>
-                : <button onClick={() => {
-                  this.props.follow(user.id)
-                }}>Follow</button>
-              }
+      <div className={classes.container}>
+        <div>
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
+          <span>4</span>
+          <span>5</span>
+        </div>
+        <div className={classes.usersContainer}>
+          {this.props.userData.map(user => <div key={user.id}>
+              <UserCard user={user}/>
             </div>
-          )
-        }
+          )}
+        </div>
       </div>
     )
   }
